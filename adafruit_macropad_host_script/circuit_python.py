74b1372d396583e0fa1a6338faa55b5bc6ea09d5
Kenny Baskett <kbaskett248@gmail.com>
2022-06-11 07:29:36 -0400

    Extract circuitpython classes

---
+++ adafruit_macropad_host_script/circuit_python.py
@@ -0,0 +1,90 @@
+import json
+import os
+import sys
+from datetime import datetime, timedelta
+from typing import Any, Dict, Iterable, Optional
+
+from adafruit_board_toolkit import circuitpython_serial
+
+from serial_device import SerialDevice
+
+
+class CircuitPythonDevice:
+    TIME_UPDATE_FREQUENCY = timedelta(hours=1)
+
+    port: circuitpython_serial.ListPortInfo
+    serial_device: SerialDevice
+
+    latest_time_update: Optional[datetime]
+    active_window: Optional[str]
+
+    def __init__(self, port: circuitpython_serial.ListPortInfo) -> None:
+        self.port = port
+        self.serial_device = SerialDevice(port.device)
+
+        self.latest_time_update = None
+        self.active_window = None
+
+    def _send_data(self, data: Dict[str, Any]) -> bool:
+        data_to_send = (json.dumps(data) + "\n\r").encode("ascii")
+        return self.serial_device.send_data(data_to_send)
+
+    def connect(self) -> bool:
+        self.serial_device.connect()
+        data = {
+            "event": "connect",
+            "host_name": os.environ["COMPUTERNAME"],
+            "host_os": sys.platform,
+        }
+        return self._send_data(data)
+
+    @property
+    def connected(self) -> bool:
+        return self.serial_device.connected
+
+    def update_time(self) -> bool:
+        current_time = datetime.now()
+        data = {"event": "sync_time", "timestamp": int(current_time.timestamp())}
+        result = self._send_data(data)
+        if result:
+            self.latest_time_update = current_time
+        return result
+
+    def update_active_window(self, active_window: str) -> bool:
+        data = {"event": "update_active_window", "active_window": active_window}
+        result = self._send_data(data)
+        if result:
+            self.active_window = active_window
+        return result
+
+    def __eq__(self, __o: object) -> bool:
+        return isinstance(__o, self.__class__) and self.port == __o.port
+
+    def __hash__(self) -> int:
+        return hash(self.port)
+
+    def needs_time_update(self) -> bool:
+        return (
+            self.latest_time_update is None
+            or datetime.now() >= self.latest_time_update + self.TIME_UPDATE_FREQUENCY
+        )
+
+    def needs_window_update(self, window: str) -> bool:
+        return self.active_window != window
+
+
+class CircuitPythonDeviceCollection:
+    devices: Dict[circuitpython_serial.ListPortInfo, CircuitPythonDevice]
+
+    def __init__(self) -> None:
+        self.devices = {}
+
+    def connect_to_new_devices(self):
+        for comport in circuitpython_serial.data_comports():
+            if comport not in self.devices:
+                new_device = CircuitPythonDevice(comport)
+                if new_device.connect():
+                    self.devices[comport] = new_device
+
+    def connected_devices(self) -> Iterable[CircuitPythonDevice]:
+        return (device for device in self.devices.values() if device.connected)
--- adafruit_macropad_host_script/script.py
+++ adafruit_macropad_host_script/script.py
@@ -1,16 +1,12 @@
-import json
 import logging
-import os
 import sys
 import time
-from datetime import datetime, timedelta
-from typing import Any, Dict, Iterable, Optional
+from typing import Optional
 
 import pygetwindow as gw
 import schedule
-from adafruit_board_toolkit import circuitpython_serial
 
-from serial_device import SerialDevice
+from circuit_python import CircuitPythonDeviceCollection
 
 formatter = logging.Formatter(
     fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
@@ -21,87 +17,6 @@ fh = logging.FileHandler("serial.log")
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
-
-
-class CircuitPythonDevice:
-    TIME_UPDATE_FREQUENCY = timedelta(hours=1)
-
-    port: circuitpython_serial.ListPortInfo
-    serial_device: SerialDevice
-
-    latest_time_update: Optional[datetime]
-    active_window: Optional[str]
-
-    def __init__(self, port: circuitpython_serial.ListPortInfo) -> None:
-        self.port = port
-        self.serial_device = SerialDevice(port.device)
-
-        self.latest_time_update = None
-        self.active_window = None
-
-    def _send_data(self, data: Dict[str, Any]) -> bool:
-        data_to_send = (json.dumps(data) + "\n\r").encode("ascii")
-        return self.serial_device.send_data(data_to_send)
-
-    def connect(self) -> bool:
-        self.serial_device.connect()
-        data = {
-            "event": "connect",
-            "host_name": os.environ["COMPUTERNAME"],
-            "host_os": sys.platform,
-        }
-        return self._send_data(data)
-
-    @property
-    def connected(self) -> bool:
-        return self.serial_device.connected
-
-    def update_time(self) -> bool:
-        current_time = datetime.now()
-        data = {"event": "sync_time", "timestamp": int(current_time.timestamp())}
-        result = self._send_data(data)
-        if result:
-            self.latest_time_update = current_time
-        return result
-
-    def update_active_window(self, active_window: str) -> bool:
-        data = {"event": "update_active_window", "active_window": active_window}
-        result = self._send_data(data)
-        if result:
-            self.active_window = active_window
-        return result
-
-    def __eq__(self, __o: object) -> bool:
-        return isinstance(__o, self.__class__) and self.port == __o.port
-
-    def __hash__(self) -> int:
-        return hash(self.port)
-
-    def needs_time_update(self) -> bool:
-        return (
-            self.latest_time_update is None
-            or datetime.now() >= self.latest_time_update + self.TIME_UPDATE_FREQUENCY
-        )
-
-    def needs_window_update(self, window: str) -> bool:
-        return self.active_window != window
-
-
-class CircuitPythonDeviceCollection:
-    devices: Dict[circuitpython_serial.ListPortInfo, CircuitPythonDevice]
-
-    def __init__(self) -> None:
-        self.devices = {}
-
-    def connect_to_new_devices(self):
-        for comport in circuitpython_serial.data_comports():
-            if comport not in self.devices:
-                new_device = CircuitPythonDevice(comport)
-                if new_device.connect():
-                    self.devices[comport] = new_device
-
-    def connected_devices(self) -> Iterable[CircuitPythonDevice]:
-        return (device for device in self.devices.values() if device.connected)
 
 
 def send_time_updates(collection: CircuitPythonDeviceCollection):
