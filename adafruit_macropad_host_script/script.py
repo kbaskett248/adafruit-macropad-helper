2738bb8249254ad447b591c2dc3635f66d517b92
Kenny Baskett <kbaskett248@gmail.com>
2022-06-11 07:24:35 -0400

    Extract SerialDevice class

--- adafruit_macropad_host_script/script.py
+++ adafruit_macropad_host_script/script.py
@@ -8,8 +8,9 @@ from typing import Any, Dict, Iterable, Optional
 
 import pygetwindow as gw
 import schedule
-import serial
 from adafruit_board_toolkit import circuitpython_serial
+
+from serial_device import SerialDevice
 
 formatter = logging.Formatter(
     fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
@@ -20,48 +21,6 @@ fh = logging.FileHandler("serial.log")
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
-
-
-class SerialDevice:
-    serial_: serial.Serial
-
-    connected: bool
-    error_count: int
-
-    MAX_ERROR_COUNT = 5
-
-    def __init__(self, port: Any, baudrate=115200, **kwargs) -> None:
-        self.connected = False
-        self.error_count = 0
-
-        self.serial_ = serial.Serial(baudrate=baudrate, **kwargs)
-        self.serial_.setPort(port)
-
-    def send_data(self, data: bytes) -> bool:
-        if not self.connected:
-            return False
-
-        logger.debug(f"{self.serial_.port} :: {data}")
-        try:
-            self.serial_.open()
-            self.serial_.write(data)
-            self.serial_.close()
-        except Exception as e:
-            logger.exception(e)
-            self.error_count += 1
-            if self.error_count > self.MAX_ERROR_COUNT:
-                self.connected = False
-                logger.error(
-                    f"{self} exceeded {self.MAX_ERROR_COUNT} errors; disconnecting device"
-                )
-            return False
-        else:
-            self.error_count = 0
-            return True
-
-    def connect(self):
-        logger.info(f"Connected to new device: {self.serial_.port}")
-        self.connected = True
 
 
 class CircuitPythonDevice:
---
+++ adafruit_macropad_host_script/serial_device.py
@@ -0,0 +1,48 @@
+import logging
+import serial
+from typing import Any
+
+
+logger = logging.getLogger(__name__)
+
+
+class SerialDevice:
+    serial_: serial.Serial
+
+    connected: bool
+    error_count: int
+
+    MAX_ERROR_COUNT = 5
+
+    def __init__(self, port: Any, baudrate=115200, **kwargs) -> None:
+        self.connected = False
+        self.error_count = 0
+
+        self.serial_ = serial.Serial(baudrate=baudrate, **kwargs)
+        self.serial_.setPort(port)
+
+    def send_data(self, data: bytes) -> bool:
+        if not self.connected:
+            return False
+
+        logger.debug(f"{self.serial_.port} :: {data}")
+        try:
+            self.serial_.open()
+            self.serial_.write(data)
+            self.serial_.close()
+        except Exception as e:
+            logger.exception(e)
+            self.error_count += 1
+            if self.error_count > self.MAX_ERROR_COUNT:
+                self.connected = False
+                logger.error(
+                    f"{self} exceeded {self.MAX_ERROR_COUNT} errors; disconnecting device"
+                )
+            return False
+        else:
+            self.error_count = 0
+            return True
+
+    def connect(self):
+        logger.info(f"Connected to new device: {self.serial_.port}")
+        self.connected = True
